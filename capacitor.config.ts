ef4e1aa35b06575fb3d57165ba57c1d387086a14
pedro <pedro_ton8@hotmail.com>
2024-03-24 01:47:30 -0300

    commitando apk android

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'avformadora',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
